3bca2047f5da9758849476dd34f166de06a13af4
Rajiv Khanduja <49587262+rajiv786@users.noreply.github.com>
2023-02-05 20:06:23 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(passport.session());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://checkhb.netlify.app",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
